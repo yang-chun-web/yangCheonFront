6fd00539892ee7df24fca0fce5e64bdb3ae55783
UiSun <114968638+RM-UISun@users.noreply.github.com>
2022-10-19 09:02:43 +0900

    페이징 및 검색

--- src/Routes/boards/List.js
+++ src/Routes/boards/List.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import Pagination from "react-js-pagination";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { paging } from "../../api";
-import Paging from "../page/Page";
 
 const Block = styled.div`
   position: fixed;
@@ -18,6 +16,7 @@ const Wrapper = styled.div`
   background-color: #ffffffee;
   height: 88vh;
   z-index: 0;
+  overflow-y: scroll;
 `;
 
 const TextCard = styled(Link)`
@@ -51,12 +50,24 @@ const List = () => {
   const [list, setList] = useState([]);
   const [page,setPage] = useState(1);
   const [total, setTotal] = useState(0);
-  const [cPage, setCpage] = useState(0);
+  const [cPage, setCpage] = useState(5);
+  const [findStr, setFindStr] = useState("");
 
 
     // 토탈값 가져오기
     const getTotal = async () => {
-      const reqData = await fetch("/board/board-paging");
+      const pageData = {
+        page : page,
+        cPage : cPage,
+        findStr : findStr,
+      };
+      const reqData = await fetch("/board/board-paging",{
+        method : "POST",
+        body: JSON.stringify(pageData),
+        headers: {
+        'Content-Type': 'application/json'
+    }
+      });
       const total = await reqData.json();
       let tn = total.length;
       setTotal(tn);
@@ -67,6 +78,8 @@ const List = () => {
       
       const pageData = {
         page : page,
+        cPage : cPage,
+        findStr : findStr,
       };
 
       const reqData = await fetch("/board/board-nextPage",{
@@ -82,11 +95,54 @@ const List = () => {
 
       setPage(page);
       setCpage(cPage);
+    }
+    
+    // 검색 글자 변경 가능
+    const onChangeFind = async(e) => {
+      setFindStr(e.target.value);
+    }
+
+    // 검색하기
+    const search = async(e) => {
+      
+      const findData = {
+        findStr : findStr,
+        page : page,
+        cPage : cPage,
+      }
+      const response = await fetch("/board/board-search",{
+        method:"POST",
+        body: JSON.stringify(findData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const body = await response.json();
+      const totpage = body.length;
+      console.log(totpage);
+      
+      getTotal();
+      setTotal(totpage);
+      setPage(page);
+      setList(body);
+
   }
-
-
+  // 처음에 리스트 출력
   const fetchList = async () => {
-    const response = await fetch("/board/board-search-list");
+    // const response = await fetch("/board/board-search-list");
+    const findData = {
+      findStr : findStr,
+      page : page,
+      cPage : cPage,
+    }
+
+    const response = await fetch("/board/board-search",{
+      method:"POST",
+      body: JSON.stringify(findData),
+        headers: {
+        'Content-Type': 'application/json'
+    }
+  });
     const body = await response.json();
     setList(body);
   };
@@ -101,6 +157,10 @@ const List = () => {
   return (
     <Block>
       <Wrapper>
+        
+              <input value={findStr} onChange={onChangeFind}></input>
+              <button onClick={(e)=>search(e)}>검색</button>
+            
         {loading
           ? ""
           : list.map((item) => (
@@ -113,7 +173,7 @@ const List = () => {
             ))}
             <Pagination
               activePage={page} // 현재 페이지
-              itemsCountPerPage={10} // 한 페이지당 보여줄 리스트 아이템의 개수 백에서도 offset변경해야 한다. 
+              itemsCountPerPage={cPage} // 한 페이지당 보여줄 리스트 아이템의 개수 백에서도 offset변경해야 한다. 
               totalItemsCount={total} // 총 아이템의 개수
               pageRangeDisplayed={5} // 페이지내에서 보여줄 페이지 범위
               prevPageText={"‹"}
