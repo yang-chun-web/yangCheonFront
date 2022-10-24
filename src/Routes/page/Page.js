433a81efa077d9d1265bb9f720d3e52868bde2fa
UiSun <114968638+RM-UISun@users.noreply.github.com>
2022-10-24 10:54:02 +0900

    페이징 컴포넌트 수정

--- src/Routes/page/Page.js
+++ src/Routes/page/Page.js
@@ -1,42 +1,64 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import Pagination from "react-js-pagination";
+import { paging } from "../../api";
 
-const Paging = () => {
+const Paging = ( ) => {
+
   const [page, setPage] = useState(1);
-  /*
-    let [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [countPage, setCountPage] = useState(5); // 한 페이지에 보여줄 아이템의 개수
+  const [rangePage, setRangePage] = useState(0); // 페이지내에서 보여줄 범위
+  const [limit, setLimit] = useState(0);
+  const [offset, setOffset] = useState(0);
+
+    // 글 목록 리스트 가져오는 api를 최대한 한번에 써서 페이징 처리를 하기. 
 
     // 토탈값 가져오기
     const getTotal = async () => {
-        const reqData = await fetch("/board/board-paging");
-        const total = await reqData.json();
-        let tn = total.length;
-        setTotal(tn);
-    } */
+        const reqData = await paging();
+        // const total = await reqData.json();
+        console.log(reqData);
+        // let totalNumber = total.length;
+        // setTotal(totalNumber);
 
-  const handlePageChange = (page) => {
-    setPage(page);
-  };
+    }
+    // 맨앞
+    const firstPage = () =>{
+      setPage(1);
 
-  // 토탈 페이지를 얻어서 넣고 다시 백앤드로 넘겨서 알맞게 페이지를 출력해준다.
-  // 토탈 획득 성공.
-  // 이제 버튼 누르면 현재 페이지 값을 넘겨서 순서에 맞게 조절
+    } 
+    // 맨끝
+    const lastPage = () =>{
+      setPage(total);
+    }
+    // 이전
+    const prevPage = () =>{
+      
+    }
+    // 다음
+    const nextPage = () =>{
+      
+    }
+    // 보여줄 페이지 계산
+
+
+
+
 
   useEffect(() => {
-    //getTotal();
+    getTotal();
   }, []);
 
   return (
-    <Pagination
-      activePage={page} // 현재 페이지
-      itemsCountPerPage={5} // 한 페이지당 보여줄 리스트 아이템의 개수
-      totalItemsCount={123} // 총 아이템의 개수
-      pageRangeDisplayed={5} // 페이지내에서 보여줄 페이지 범위
-      prevPageText={"<"}
-      nextPageText={"›"}
-      onChange={handlePageChange} // 페이지가 바뀔 때 핸들링해주는 함수
-    />
+    <div>
+      <button>맨앞</button>
+      <button>이전</button>
+
+      <button>페이지</button>
+
+      <button>다음</button>
+      <button>맨끝</button>
+    </div>
   );
 };
 
